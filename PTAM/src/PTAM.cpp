d54a7eee177ab62aca14c4d51abad50a349bd6b3
Sergei <sergey.belousov@itseez.com>
2014-06-19 01:04:07 +0400

    add OpenCV wrapper

---
+++ PTAM/src/PTAM.cpp
@@ -0,0 +1,71 @@
+#include "PTAM.h"
+#include "image.h"
+
+namespace ptam {
+
+PTAM::PTAM(cv::Size& frameSize) {
+    mCamera = new ATANCamera();
+    mMap = new Map;
+    mMapMaker = new MapMaker(*mMap, *mCamera);
+    mSize = frameSize;
+    mTracker = new Tracker(CVD::ImageRef(mSize.width, mSize.height), *mCamera, *mMap, *mMapMaker);
+    mCvdFrame = CVD::ImageRef(mSize.width, mSize.height);
+}
+
+PTAM::~PTAM() {
+}
+
+void PTAM::process(const cv::Mat& frame) {
+    // Grab video frame in black and white from videobuffer
+    convertOpenCVToCvdByte(frame, mCvdFrame);
+    mTracker->TrackFrame(mCvdFrame);
+}
+
+void PTAM::startTracking() {
+    mTracker->StartTracking();
+}
+
+void PTAM::stopTracking() {
+    mTracker->StopTracking();
+}
+
+std::vector<float> PTAM::getPose() {
+    SE3<> se3pose = mTracker->GetCurrentPose();
+    std::vector<float> pose(7);
+    Vector<3> translation, rotation;
+
+    translation = se3pose.get_translation();
+    rotation = se3pose.get_rotation().ln();
+    pose[0]=translation[0];
+    pose[1]=translation[1];
+    pose[2]=translation[2];
+    pose[3]=norm(rotation) / 3.14159 * 360.0;
+    pose[4]=rotation[0] / pose[3];
+    pose[5]=rotation[1] / pose[3];
+    pose[6]=rotation[2] / pose[3];
+    return pose;
+}
+
+void PTAM::convertOpenCVToCvdByte(const cv::Mat& in, CVD::Image<CVD::byte>& out) {
+    cv::Mat clone = in.clone();
+    cv::Mat_<cv::Vec3b>& frame_p = (cv::Mat_<cv::Vec3b>&)clone;
+    for (int i = 0; i < in.size().height; i++) {
+        for (int j = 0; j < in.size().width; j++) {
+            out[i][j] = (frame_p(i,j)[0] + frame_p(i,j)[1] + frame_p(i,j)[2]) / 3;
+        }
+    }
+}
+
+void PTAM::convertOpenCVToCvdRGB(const cv::Mat& in, CVD::Image<CVD::Rgb<CVD::byte>>& out) {
+    cv::Mat clone = in.clone();
+    cv::Mat_<cv::Vec3b>& frame_p = (cv::Mat_<cv::Vec3b>&)clone;
+    for (int i = 0; i < in.size().height; i++) {
+        for (int j = 0; j < in.size().width; j++) {
+            out[i][j].red = frame_p(i,j)[2];
+            out[i][j].green = frame_p(i,j)[1];
+            out[i][j].blue = frame_p(i,j)[0];
+        }
+    }
+}
+
+}
---
+++ PTAM/src/PTAM.h
@@ -0,0 +1,36 @@
+#ifndef __PTAM_H__
+#define __PTAM_H__
+
+#include "Tracker.h"
+#include "ATANCamera.h"
+#include "Map.h"
+#include "MapMaker.h"
+#include "opencv2/core/core.hpp"
+
+namespace ptam {
+
+class PTAM {
+public:
+    PTAM(cv::Size& frameSize);
+    ~PTAM();
+
+    void process(const cv::Mat& frame);
+    void startTracking();
+    void stopTracking();
+
+    std::vector<float> getPose();
+private:
+    Map *mMap;
+    MapMaker *mMapMaker;
+    Tracker *mTracker;
+    ATANCamera *mCamera;
+    CVD::Image<CVD::byte> mCvdFrame;
+    cv::Size mSize;
+
+    void convertOpenCVToCvdByte(const cv::Mat& in, CVD::Image<CVD::byte>& out);
+    void convertOpenCVToCvdRGB(const cv::Mat& in, CVD::Image<CVD::Rgb<CVD::byte>>& out);
+};
+
+}
+
+#endif /*__PTAM_H__*/
